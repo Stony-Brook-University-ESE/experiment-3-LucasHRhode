7d9cac49d90c14b6a0e907d8745069c03124328e
LucasHRhode <lucas.rhode@stonybrook.edu>
2025-05-14 13:22:20 -0400

    +Solved Experiment 3

--- Build/Experiment3.c
+++ Build/Experiment3.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int n = 1000;
-    int matrix[n][n];
-
+    int *matrix = malloc(n * n * sizeof(int));
+    if (matrix == NULL)
+ {
+    printf("Memory allocation failed.\n");
+    return 1;
+ }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            matrix[i][j] = i * n + j + 1;
+            matrix[i * n + j] = i * n + j + 1;
         }
+
     }
 
     for (int i = n - 1; i >= 0; i--) {
         for (int j = n - 1; j >= 0; j--) {
-            printf("%4d ", matrix[i][j]);
+            printf("%4d \n ", matrix[i * n + j]);
         }
         printf("\n");
     }
 
+    free(matrix);
     return 0;
 }
